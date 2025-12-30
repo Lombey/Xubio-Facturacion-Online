075cd63d271d2dce404b65e978237949a894475f
Martin <martin.lombardi@gmail.com>
2025-12-30 15:06:01 -0300

    flashv5

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     })
   ],
   build: {
-    outDir: 'dist',
+    outDir: resolve(__dirname, 'test-imprimir-pdf/dist'),
     assetsDir: 'assets',
     rollupOptions: {
       input: {
